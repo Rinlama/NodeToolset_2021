36cc882847836b98ff377913075155159ce77d2c
rinlama <rinjinlama83@gmail.com>
2021-09-04 08:11:29 -0400

    commit

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "./public") });
+  //res.sendFile("index.html", { root: path.join(__dirname, "./public") });
+  const data = {
+    customer: ["Javok33", "Javok3", "Javok"],
+  };
+  res.render("index", data);
 });
 
 app.listen(3001, () => {
